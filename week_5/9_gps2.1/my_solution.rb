7c64b687c06a585e72ddbc0a0fcacf231511611a
bootcoder <bootcoder@gmail.com>
2014-04-13 18:04:36 -0400

    update comments

--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -9,98 +9,98 @@
 
 
 
-# Initial Code with comments
-# 
-def bakery_num(num_of_people, fav_food) #function definition takes two args
-  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1} #hash available foods
- # pie_qty = 0
-  #cake_qty = 0
- # cookie_qty = 0
+# # Initial Code with comments
+# # 
+# def bakery_num(num_of_people, fav_food) #function definition takes two args
+#   my_list = {"pie" => 8, "cake" => 6, "cookie" => 1} #hash available foods
+#  # pie_qty = 0
+#   #cake_qty = 0
+#  # cookie_qty = 0
   
-  has_fave = false
+#   has_fave = false
 
-  my_list.each_key do |k| #iterate through the hash
-    if k == fav_food #key is fav food
-      has_fave = true # changes boolean for has fav
-      fav_food = k # assigns fav food variable to hash key
-    end
-  end
-  if has_fave == false #if statement for no fav food
-    raise ArgumentError.new("You can't make that food") #raises error
-  else
-    fav_food_qty = my_list.values_at(fav_food)[0] #sets fav food quantity variable, it is assigned to value at fav food (value from my_list hash)
-      if num_of_people % fav_food_qty == 0 #this takes number of people argument modulus fav food quant equals 0
-        num_of_food = num_of_people / fav_food_qty #set num_food variable equal to num people / fav food quantity
-        return "You need to make #{num_of_food} #{fav_food}(s)." # returns string quantity of food and which food
-      else num_of_people % fav_food_qty != 0 #else if the number of people % fav quant != 0 run the while loop below
-        while num_of_people > 0 # while number of people is > 0
-          if num_of_people / my_list["pie"] > 0 # if the number of people divided 
-          # by the hash value of pie > 0
-            pie_qty = num_of_people / my_list["pie"] #sets pie quantity to the number 
-            # of people divided by the hash value of pie.
-            num_of_people = num_of_people % my_list["pie"]#set the number of people 
-            # vairable to the number of people to the number of people % my_list hash value pie
-          elsif num_of_people / my_list["cake"] > 0 # elsif the number of people divided by
-          # my_list cake value is > 0 do
-            cake_qty = num_of_people / my_list["cake"] #sets cake quantiy to the number of people divided by 
-            #my-list hash value cake
-            num_of_people = num_of_people % my_list["cake"] # sets number of people to 
-            #number of people % value of my_hash cake
-          else
-            cookie_qty = num_of_people # sets cookie qty to num of people remaining
-            num_of_people = 0 # sets number of people to 0
-          end
-        end
-        # returns variables in string interpolation from above.
-        return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
-    end
-  end
-end
-
-
+#   my_list.each_key do |k| #iterate through the hash
+#     if k == fav_food #key is fav food
+#       has_fave = true # changes boolean for has fav
+#       fav_food = k # assigns fav food variable to hash key
+#     end
+#   end
+#   if has_fave == false #if statement for no fav food
+#     raise ArgumentError.new("You can't make that food") #raises error
+#   else
+#     fav_food_qty = my_list.values_at(fav_food)[0] #sets fav food quantity variable, it is assigned to value at fav food (value from my_list hash)
+#       if num_of_people % fav_food_qty == 0 #this takes number of people argument modulus fav food quant equals 0
+#         num_of_food = num_of_people / fav_food_qty #set num_food variable equal to num people / fav food quantity
+#         return "You need to make #{num_of_food} #{fav_food}(s)." # returns string quantity of food and which food
+#       else num_of_people % fav_food_qty != 0 #else if the number of people % fav quant != 0 run the while loop below
+#         while num_of_people > 0 # while number of people is > 0
+#           if num_of_people / my_list["pie"] > 0 # if the number of people divided 
+#           # by the hash value of pie > 0
+#             pie_qty = num_of_people / my_list["pie"] #sets pie quantity to the number 
+#             # of people divided by the hash value of pie.
+#             num_of_people = num_of_people % my_list["pie"]#set the number of people 
+#             # vairable to the number of people to the number of people % my_list hash value pie
+#           elsif num_of_people / my_list["cake"] > 0 # elsif the number of people divided by
+#           # my_list cake value is > 0 do
+#             cake_qty = num_of_people / my_list["cake"] #sets cake quantiy to the number of people divided by 
+#             #my-list hash value cake
+#             num_of_people = num_of_people % my_list["cake"] # sets number of people to 
+#             #number of people % value of my_hash cake
+#           else
+#             cookie_qty = num_of_people # sets cookie qty to num of people remaining
+#             num_of_people = 0 # sets number of people to 0
+#           end
+#         end
+#         # returns variables in string interpolation from above.
+#         return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+#     end
+#   end
+# end
 
 
-# Mess around revision.
 
-def bakery_num(num_of_people, fav_food)
-  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
-  pie_qty = 0
-  cake_qty = 0
-  cookie_qty = 0
+
+# # Mess around revision.
+
+# def bakery_num(num_of_people, fav_food)
+#   my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+#   pie_qty = 0
+#   cake_qty = 0
+#   cookie_qty = 0
   
-  has_fave = false
+#   has_fave = false
 
-  my_list.each_key do |k| 
-    if k == fav_food 
-      has_fave = true 
-    else
-      raise ArgumentError.new("You can't make that food") 
-    end
+#   my_list.each_key do |k| 
+#     if k == fav_food 
+#       has_fave = true 
+#     else
+#       raise ArgumentError.new("You can't make that food") 
+#     end
   
-    fav_food_qty = my_list.values_at(fav_food) 
-      if num_of_people % fav_food_qty == 0
-        num_of_food = num_of_people / fav_food_qty 
-        return "You need to make #{num_of_food} #{fav_food}(s)."
-      else
-        case fav_food
-        when fav_food == "pie"
+#     fav_food_qty = my_list.values_at(fav_food) 
+#       if num_of_people % fav_food_qty == 0
+#         num_of_food = num_of_people / fav_food_qty 
+#         return "You need to make #{num_of_food} #{fav_food}(s)."
+#       else
+#         case fav_food
+#         when fav_food == "pie"
 
-          while num_of_people > 0 
-            if num_of_people / my_list["pie"] > 0
-            pie_qty = num_of_people / my_list["pie"] 
-            num_of_people = num_of_people % my_list["pie"]
-          elsif num_of_people / my_list["cake"] > 0 
-            cake_qty = num_of_people / my_list["cake"] 
-            num_of_people = num_of_people % my_list["cake"] 
-          else
-            cookie_qty = num_of_people 
-            num_of_people = 0
-          end
-        end
-        return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
-    end
-  end
-end
+#           while num_of_people > 0 
+#             if num_of_people / my_list["pie"] > 0
+#             pie_qty = num_of_people / my_list["pie"] 
+#             num_of_people = num_of_people % my_list["pie"]
+#           elsif num_of_people / my_list["cake"] > 0 
+#             cake_qty = num_of_people / my_list["cake"] 
+#             num_of_people = num_of_people % my_list["cake"] 
+#           else
+#             cookie_qty = num_of_people 
+#             num_of_people = 0
+#           end
+#         end
+#         return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+#     end
+#   end
+# end
 
 
 
